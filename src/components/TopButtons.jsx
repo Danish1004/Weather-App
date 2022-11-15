b5c27ca6dc3b74f2eca2d6e74cc360a37440fe6c
Danish Sharma <danishsharma13255@gmail.com>
2022-11-16 03:52:43 +0530

    final commit

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function TopButtons() {
+function TopButtons({ setQuery }) {
   const cities = [
     {
       id: 1,
-      title: "London",
+      title: "Delhi",
     },
     {
       id: 2,
@@ -26,7 +26,11 @@ function TopButtons() {
   return (
     <div className="flex items-center justify-around my-6">
       {cities.map((city) => (
-        <button key={city.id} className="text-white text-lg font-medium ">
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium "
+          onClick={() => setQuery({ q: city.title })}
+        >
           {city.title}
         </button>
       ))}
